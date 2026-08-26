0e85a3b6b86c2132a5e9a49c8f11383e883bfb11
AbdulRauf <mrrauf228@gmail.com>
2026-08-26 22:14:04 +0500

    tested code and the same code brought back

--- loadbook/lib/main.dart
+++ loadbook/lib/main.dart
@@ -1,27 +1,27 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// import 'app/theme.dart';
-// import 'data/local/database.dart';
-// import 'features/daily/screens/daily_screen.dart';
+import 'app/theme.dart';
+import 'data/local/database.dart';
+import 'features/daily/screens/daily_screen.dart';
 
-// void main() {
-//   WidgetsFlutterBinding.ensureInitialized();
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
 
-//   runApp(const LoadBookApp());
-// }
+  runApp(const LoadBookApp());
+}
 
-// class LoadBookApp extends StatelessWidget {
-//   const LoadBookApp({super.key});
+class LoadBookApp extends StatelessWidget {
+  const LoadBookApp({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final database = LoadBookDatabase();
+  @override
+  Widget build(BuildContext context) {
+    final database = LoadBookDatabase();
 
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'LoadBook',
-//       theme: AppTheme.lightTheme,
-//       home: DailyScreen(database: database),
-//     );
-//   }
-// }
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'LoadBook',
+      theme: AppTheme.lightTheme,
+      home: DailyScreen(database: database),
+    );
+  }
+}
